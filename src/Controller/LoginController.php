b97a1ee72e7964b3e4142818edb7ef47eec0220f
xamayca <christopher.hariouat@gmail.com>
2024-11-05 10:51:02 +0100

    form twig ok for login & registration

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,10 +13,12 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        $form = $this->createForm(LoginFormType::class);
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/index.html.twig', [
+            'loginForm' => $form,
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
